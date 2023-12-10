781f000fdd5c4c012f8932f86997ee45c83ef3d8
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-12-10 16:55:22 +0000

    Update DamageZone.cs

--- code/DamageZone.cs
+++ code/DamageZone.cs
@@ -7,7 +7,7 @@ public class DamageZone : MonoBehaviour
     void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
-
+        // ef ruby snertir þá minnkar health um 1
         if (controller != null)
         {
             controller.ChangeHealth(-1);
