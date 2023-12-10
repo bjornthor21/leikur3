dcefd1d945261e164600ef47c5716316ac0a3a39
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-12-10 17:00:29 +0000

    Update NonPlayerCharacter.cs

--- code/NonPlayerCharacter.cs
+++ code/NonPlayerCharacter.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
+    // lengd sýnitíma textans
     public float displayTime = 4.0f;
     public GameObject dialogBox;
     float timerDisplay;
 
     void Start()
     {
+        // byrjar á að fela diologBox
         dialogBox.SetActive(false);
+        // -1 í tímanum
         timerDisplay = -1.0f;
     }
 
     void Update()
     {
+        // ef tíminn er meira eða = 0
         if (timerDisplay >= 0)
         {
+            // tímin lækkar
             timerDisplay -= Time.deltaTime;
+            // ef timerinn er minna en 0 þá hverfur tíminn aftur
             if (timerDisplay < 0)
             {
                 dialogBox.SetActive(false);
@@ -28,7 +34,9 @@ public class NonPlayerCharacter : MonoBehaviour
 
     public void DisplayDialog()
     {
+        // timerinn endursettur
         timerDisplay = displayTime;
+        // sýnt textan
         dialogBox.SetActive(true);
     }
 }
