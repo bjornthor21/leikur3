02362383513db6777e4d8551aec7733ad3f15144
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-12-10 16:57:32 +0000

    Update Projectile.cs

--- code/Projectile.cs
+++ code/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    // geumir rigidbody skotsins
     Rigidbody2D rigidbody2d;
 
     void Awake()
@@ -11,6 +12,7 @@ public class Projectile : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
+    // public fall sem skýtur skotinu í gefna átt með gefnu afli
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
@@ -18,20 +20,25 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
+        // athugar ef skotið er langt frá upphaflegu staðsetningu ef svo eyðir skotinu
         if (transform.position.magnitude > 1000.0f)
         {
             Destroy(gameObject);
         }
     }
 
+    // collision detection sem er kallað í þegar skotið snertir annan rigidbody collider
     void OnCollisionEnter2D(Collision2D other)
     {
+        // ef colliderinn sem skotið hitti er óvinur
         EnemyController e = other.collider.GetComponent<EnemyController>();
         if (e != null)
         {
+            // þá er Fix fallið í óvina scriptunni kallað
             e.Fix();
         }
 
+        // eyðir alltaf skotinu þegar það snerir annan collider
         Destroy(gameObject);
     }
 }
