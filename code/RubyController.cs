c64ed577d3e16d6d0c13f165e6281ba08e186398
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-12-10 17:19:29 +0000

    Update RubyController.cs

--- code/RubyController.cs
+++ code/RubyController.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    // Public breytur fyrir hreyfi hraða, heildar líf, skot, og audio clip.
     public float speed = 3.0f;
-
     public int maxHealth = 5;
-
     public GameObject projectilePrefab;
-
     public AudioClip shootClip;
 
+    // nær í núverandi líf
     public int health { get { return currentHealth; } }
     int currentHealth;
-
+    
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
@@ -22,41 +21,40 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
     AudioSource audioSource;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
-
         currentHealth = maxHealth;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        // fær input fyrir hreyfingu.
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        // býr til vector úr input
         Vector2 move = new Vector2(horizontal, vertical);
 
+        // breytir look direction
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
 
+        // breytir animation
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
+        // ósýnileiki timer teljari.
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -64,18 +62,22 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
+        // skjóta logic.
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
             this.PlaySound(shootClip);
         }
 
+        // ef ýtt er á x.
         if (Input.GetKeyDown(KeyCode.X))
         {
+            // og raycastið hittir froskinn
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
                 NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                // þá er kallað á froskinn til að opna textan
                 if (character != null)
                 {
                     character.DisplayDialog();
@@ -86,6 +88,7 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        // hreyfir ruby
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
@@ -93,13 +96,16 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
+    // spilar hljóð
     public void PlaySound(AudioClip clip)
     {
         audioSource.PlayOneShot(clip);
     }
 
+    // breytir lífi ruby
     public void ChangeHealth(int amount)
     {
+        // ef ruby er ósýnileg þá minnkar ekki líf
         if (amount < 0)
         {
             if (isInvincible)
@@ -109,11 +115,13 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
 
+        // annars minnkar líf
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-
+        // og teljarinn minnkar
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
+    // skýtur skoti
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
