6b93888129df0d8b0907e08b2b7458c915239968
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-12-10 17:10:52 +0000

    Update HealthCollectible.cs

--- code/HealthCollectible.cs
+++ code/HealthCollectible.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
 
+    // hljóð sem er spilað þegar player tekur upp healthcollectible
     public AudioClip collectedClip;
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
+        // ef player snertir health collectible
         if (controller != null)
         {
+            // og líf ruby er minna en maxhealth semsagt búinn að missa einhver líf
             if (controller.health < controller.maxHealth)
             {
+                // hækkar líf ruby um 1, eyðir object og spilar hljóð
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
                 controller.PlaySound(collectedClip);
